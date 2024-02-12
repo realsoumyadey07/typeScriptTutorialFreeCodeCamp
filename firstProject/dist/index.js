7878b4441d2c9ad267c3f7f419695dc10fe9629f
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-02-12 14:51:38 +0530

    on going

--- firstProject/dist/index.js
+++ firstProject/dist/index.js
@@ -1,10 +1,15 @@
 "use strict";
 class User {
     constructor(email, name) {
+        this.email = email;
         this.city = "Kolkata";
         this.email = email;
         this.name = name;
     }
+    getEmail() {
+        console.log(this.email);
+    }
 }
 const per1 = new User('soumya@gmail.com', "soumya dey");
-console.log(per1);
+// console.log(per1);
+console.log(per1.getEmail());
--- firstProject/src/index.ts
+++ firstProject/src/index.ts
@@ -1,13 +1,19 @@
 class User {
-     email: string;
-     name: string;
-     readonly city: string = "Kolkata";
-     constructor(email: string, name: string){
+     private email: string;
+     public name: string;
+     private readonly city: string = "Kolkata";
+     constructor(private email: string, name: string){
           this.email = email;
           this.name = name;
      }
+     getEmail(): void{
+          console.log(this.email);
+     }
 }
 
+
 const per1 = new User('soumya@gmail.com', "soumya dey");
-console.log(per1);
+// console.log(per1);
+console.log(per1.getEmail());
+
 
